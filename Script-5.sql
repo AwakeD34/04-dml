81344261dfd207f619d597cc0df6628802e6fa38
Neto test <whaterser@mail.ru>
2024-08-10 11:57:56 +0300

    Work_2s

---
+++ Script-5.sql
@@ -0,0 +1,9 @@
+SELECT title, duration FROM Tracks ORDER BY duration DESC LIMIT 1;
+
+SELECT title FROM Tracks WHERE duration >= 210;
+
+SELECT title FROM Playlists WHERE release_year BETWEEN 2018 AND 2020;
+
+SELECT name FROM Artists WHERE name NOT LIKE '% %';
+
+SELECT title FROM Tracks WHERE title LIKE '%мой%' OR title LIKE '%my%';
